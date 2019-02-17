ae76eb68fb50f60d4bececb9aed985c040de6748
leonardo <l.frioli@studenti.unipi.it>
2019-02-17 11:24:07 +0100

    Working on question detection.

--- answer_bot.py
+++ answer_bot.py
@@ -24,19 +24,20 @@ import cv2
 import os
 import pyscreenshot as Imagegrab
 import sys
-import wx
+# import wx
 from halo import Halo
+
 
 # for terminal colors 
 class bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
-	OKGREEN = '\033[92m'
-	WARNING = '\033[93m'
-	FAIL = '\033[91m'
-	ENDC = '\033[0m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # sample questions from previous games
 sample_questions = {}
@@ -45,252 +46,324 @@ sample_questions = {}
 remove_words = []
 
 # negative words
-negative_words= []
+negative_words = []
 
 # GUI interface 
-def gui_interface():
-	app = wx.App()
-	frame = wx.Frame(None, -1, 'win.py')
-	frame.SetDimensions(0,0,640,480)
-	frame.Show()
-	app.MainLoop()
-	return None
+# def gui_interface():
+# 	app = wx.App()
+# 	frame = wx.Frame(None, -1, 'win.py')
+# 	frame.SetDimensions(0,0,640,480)
+# 	frame.Show()
+# 	app.MainLoop()
+# 	return None
 
-# load sample questions
+
 def load_json():
-	global remove_words, sample_questions, negative_words
-	remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
-	negative_words = json.loads(open("Data/settings.json").read())["negative_words"]
-	sample_questions = json.loads(open("Data/questions.json").read())
+    """ Loads a list of words to be removed and negative words from a json file to a variable.
+    It loads also questions from the previous games.
+    """
+    global remove_words, sample_questions, negative_words
+    remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
+    negative_words = json.loads(open("Data/settings.json").read())["negative_words"]
+    sample_questions = json.loads(open("Data/questions.json").read())
 
-# take screenshot of question 
+
 def screen_grab(to_save):
-	# 31,228 485,620 co-ords of screenshot// left side of screen
-	im = Imagegrab.grab(bbox=(31,228,485,640))
-	im.save(to_save)
+    """ Takes a screenshot and saves it."""
+    # 31,228 485,620 co-ords of screenshot// left side of screen
+    im = Imagegrab.grab(bbox=(31,228,485,640))
+    im.save(to_save)
+
+
+def crop_image(path):
+    image = cv2.imread(path)
+    #c v2.imshow("Original Image", image)
+    # cv2.waitKey(5000)
+
+    height, width = image.shape[:2]
+
+    # GETTING THE QUESTION
+    # Let's get the starting pixel coordiantes (top left of cropped top)
+    start_row, start_col = int(height * 23/100), int(0)
+    # Let's get the ending pixel coordinates (bottom right of cropped top)
+    end_row, end_col = int(height * 43/100), int(width)
+    cropped_img = image[start_row:end_row, start_col:end_col]
+
+    # cv2.imshow("Question", cropped_top)
+    # cv2.waitKey(3000)
+    cv2.imwrite("Screens/question.png", cropped_img)
+
+    # GETTING ANSWER 1
+    start_row, start_col = int(height * 44/100), int(0)
+    end_row, end_col = int(height * 55/100), int(width)
+
+    cropped_img = image[start_row:end_row, start_col:end_col]
+
+    # cv2.imshow("Answer 1", cropped_bot)
+    # cv2.waitKey(3000)
+    cv2.imwrite("Screens/answer1.png", cropped_img)
+
+    # GETTING ANSWER 2
+    start_row, start_col = int(height * 55 / 100), int(0)
+    end_row, end_col = int(height * 66 / 100), int(width) # + 11 from the top
+
+    cropped_img = image[start_row:end_row, start_col:end_col]
+
+    # cv2.imshow("Answer 2", cropped_bot)
+    # cv2.waitKey(3000)
+    cv2.imwrite("Screens/answer2.png", cropped_img)
+
+    # GETTING ANSWER 3
+    start_row, start_col = int(height * 66 / 100), int(0)
+    end_row, end_col = int(height * 77 / 100), int(width)
+
+    cropped_img = image[start_row:end_row, start_col:end_col]
+
+    # cv2.imshow("Answer 3", cropped_bot)
+    # cv2.waitKey(3000)
+    cv2.imwrite("Screens/answer3.png", cropped_img)
+
+    # cv2.destroyAllWindows()
+    return ["Screens/question.png", "Screens/answer1.png", "Screens/answer2.png", "Screens/answer3.png"]
+
+
+def apply_pytesseract(image):
+    # prepare argparse
+    ap = argparse.ArgumentParser(description='HQ_Bot')
+    ap.add_argument("-i", "--image", required=False, default=image, help="path to input image to be OCR'd")
+    ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
+    args = vars(ap.parse_args())
+
+    # load the image
+    image = cv2.imread(args["image"])
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    if args["preprocess"] == "thresh":
+        gray = cv2.threshold(gray, 0, 255,
+                             cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    elif args["preprocess"] == "blur":
+        gray = cv2.medianBlur(gray, 3)
+
+    # store grayscale image as a temp file to apply OCR
+    filename = "Screens/{}.png".format(os.getpid())
+    cv2.imwrite(filename, gray)
+
+    # load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
+    text = pytesseract.image_to_string(Image.open(filename), lang='ita')
+    # os.remove(filename)
+    # os.remove(screenshot_file)
+
+    return text
 
 # get OCR text //questions and options
 def read_screen():
-	spinner = Halo(text='Reading screen', spinner='bouncingBar')
-	spinner.start()
-	screenshot_file="Screens/to_ocr.png"
-	screen_grab(screenshot_file)
+    # spinner = Halo(text='Reading screen', spinner='bouncingBar')
+    # spinner.start()
+    print("Taking the screen shot....")
+    screenshot_file = "Screens/to_ocr.png"
+    #screen_grab(screenshot_file)
 
-	#prepare argparse
-	ap = argparse.ArgumentParser(description='HQ_Bot')
-	ap.add_argument("-i", "--image", required=False,default=screenshot_file,help="path to input image to be OCR'd")
-	ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
-	args = vars(ap.parse_args())
+    # temporary file used for testing
+    screenshot_file = "Screens/livequiz0.jpg"
 
-	# load the image 
-	image = cv2.imread(args["image"])
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    question_and_answers = crop_image(screenshot_file)
+    for img in question_and_answers:
+        text = apply_pytesseract(img)
+        print(text)
 
-	if args["preprocess"] == "thresh":
-		gray = cv2.threshold(gray, 0, 255,
-			cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-	elif args["preprocess"] == "blur":
-		gray = cv2.medianBlur(gray, 3)
 
-	# store grayscale image as a temp file to apply OCR
-	filename = "Screens/{}.png".format(os.getpid())
-	cv2.imwrite(filename, gray)
+    # show the output images
+    # cv2.imshow("Image", image)
+    # cv2.imshow("Output", gray)
+    # os.remove(screenshot_file)
+    # if cv2.waitKey(0):
+    #     cv2.destroyAllWindows()
+    # print(text)
 
-	# load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
-	text = pytesseract.image_to_string(Image.open(filename))
-	os.remove(filename)
-	os.remove(screenshot_file)
-	
-	# show the output images
+    # spinner.succeed()
+    # spinner.stop()
 
-	'''cv2.imshow("Image", image)
-	cv2.imshow("Output", gray)
-	os.remove(screenshot_file)
-	if cv2.waitKey(0):
-		cv2.destroyAllWindows()
-	print(text)
-	'''
-	spinner.succeed()
-	spinner.stop()
-	return text
+    exit()  # TODO delete this
+    return text
+
 
 # get questions and options from OCR text
 def parse_question():
-	text = read_screen()
-	lines = text.splitlines()
-	question = ""
-	options = list()
-	flag=False
+    text = read_screen()
+    lines = text.splitlines()
+    question = ""
+    options = list()
+    flag=False
 
-	for line in lines :
-		if not flag :
-			question=question+" "+line
-		
-		if '?' in line :
-			flag=True
-			continue
-		
-		if flag :
-			if line != '' :
-				options.append(line)
-			
-	return question, options
+    for line in lines :
+        if not flag :
+            question=question+" "+line
+
+        if '?' in line :
+            flag=True
+            continue
+
+        if flag :
+            if line != '' :
+                options.append(line)
+
+    return question, options
 
 # simplify question and remove which,what....etc //question is string
 def simplify_ques(question):
-	neg=False
-	qwords = question.lower().split()
-	if [i for i in qwords if i in negative_words]:
-		neg=True
-	cleanwords = [word for word in qwords if word.lower() not in remove_words]
-	temp = ' '.join(cleanwords)
-	clean_question=""
-	#remove ?
-	for ch in temp: 
-		if ch!="?" or ch!="\"" or ch!="\'":
-			clean_question=clean_question+ch
+    neg=False
+    qwords = question.lower().split()
+    if [i for i in qwords if i in negative_words]:
+        neg=True
+    cleanwords = [word for word in qwords if word.lower() not in remove_words]
+    temp = ' '.join(cleanwords)
+    clean_question=""
+    #remove ?
+    for ch in temp:
+        if ch!="?" or ch!="\"" or ch!="\'":
+            clean_question=clean_question+ch
 
-	return clean_question.lower(),neg
+    return clean_question.lower(),neg
 
 
 # get web page
 def get_page(link):
-	try:
-		if link.find('mailto') != -1:
-			return ''
-		req = urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
-		html = urllib2.urlopen(req).read()
-		return html
-	except (urllib2.URLError, urllib2.HTTPError, ValueError) as e:
-		return ''
+    try:
+        if link.find('mailto') != -1:
+            return ''
+        req = urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
+        html = urllib2.urlopen(req).read()
+        return html
+    except (urllib2.URLError, urllib2.HTTPError, ValueError) as e:
+        return ''
 
 # split the string
 def split_string(source):
-	splitlist = ",!-.;/?@ #"
-	output = []
-	atsplit = True
-	for char in source:
-		if char in splitlist:
-			atsplit = True
-		else:
-			if atsplit:
-				output.append(char)
-				atsplit = False
-			else:
-				output[-1] = output[-1] + char
-	return output
+    splitlist = ",!-.;/?@ #"
+    output = []
+    atsplit = True
+    for char in source:
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                output[-1] = output[-1] + char
+    return output
 
 # normalize points // get rid of common appearances // "quote" wiki option + ques
 def normalize():
-	return None	
+    return None
 
 # take screen shot of screen every 2 seconds and check for question
 def check_screen():
-	return None
+    return None
 
 # wait for certain milli seconds 
 def wait(msec):
-	return None
+    return None
 
 # answer by combining two words
 def smart_answer(content,qwords):
-	zipped= zip(qwords,qwords[1:])
-	points=0
-	for el in zipped :
-		if content.count(el[0]+" "+el[1])!=0 :
-			points+=1000
-	return points
+    zipped= zip(qwords,qwords[1:])
+    points=0
+    for el in zipped :
+        if content.count(el[0]+" "+el[1])!=0 :
+            points+=1000
+    return points
 
 # use google to get wiki page
 def google_wiki(sim_ques, options, neg):
-	spinner = Halo(text='Googling and searching Wikipedia', spinner='dots2')
-	spinner.start()
-	num_pages = 1
-	points = list()
-	content = ""
-	maxo=""
-	maxp=-sys.maxsize
-	words = split_string(sim_ques)
-	for o in options:
-		
-		o = o.lower()
-		original=o
-		o += ' wiki'
+    spinner = Halo(text='Googling and searching Wikipedia', spinner='dots2')
+    spinner.start()
+    num_pages = 1
+    points = list()
+    content = ""
+    maxo=""
+    maxp=-sys.maxsize
+    words = split_string(sim_ques)
+    for o in options:
 
-		# get google search results for option + 'wiki'
-		search_wiki = google.search(o, num_pages)
+        o = o.lower()
+        original=o
+        o += ' wiki'
 
-		link = search_wiki[0].link
-		content = get_page(link)
-		soup = BeautifulSoup(content,"lxml")
-		page = soup.get_text().lower()
+        # get google search results for option + 'wiki'
+        search_wiki = google.search(o, num_pages)
 
-		temp=0
+        link = search_wiki[0].link
+        content = get_page(link)
+        soup = BeautifulSoup(content,"lxml")
+        page = soup.get_text().lower()
 
-		for word in words:
-			temp = temp + page.count(word)
-		temp+=smart_answer(page, words)
-		if neg:
-			temp*=-1
-		points.append(temp)
-		if temp>maxp:
-			maxp=temp
-			maxo=original
-	spinner.succeed()
-	spinner.stop()
-	return points,maxo
+        temp=0
+
+        for word in words:
+            temp = temp + page.count(word)
+        temp+=smart_answer(page, words)
+        if neg:
+            temp*=-1
+        points.append(temp)
+        if temp>maxp:
+            maxp=temp
+            maxo=original
+    spinner.succeed()
+    spinner.stop()
+    return points,maxo
 
 
 # return points for sample_questions
 def get_points_sample():
-	simq = ""
-	x = 0
-	for key in sample_questions:
-		x = x + 1
-		points = []
-		simq,neg = simplify_ques(key)
-		options = sample_questions[key]
-		simq = simq.lower()
-		maxo=""
-		points, maxo = google_wiki(simq, options,neg)
-		print("\n" + str(x) + ". " + bcolors.UNDERLINE + key + bcolors.ENDC + "\n")
-		for point, option in zip(points, options):
-			if maxo == option.lower():
-				option=bcolors.OKGREEN+option+bcolors.ENDC
-			print(option + " { points: " + bcolors.BOLD + str(point) + bcolors.ENDC + " }\n")
+    simq = ""
+    x = 0
+    for key in sample_questions:
+        x = x + 1
+        points = []
+        simq,neg = simplify_ques(key)
+        options = sample_questions[key]
+        simq = simq.lower()
+        maxo=""
+        points, maxo = google_wiki(simq, options,neg)
+        print("\n" + str(x) + ". " + bcolors.UNDERLINE + key + bcolors.ENDC + "\n")
+        for point, option in zip(points, options):
+            if maxo == option.lower():
+                option=bcolors.OKGREEN+option+bcolors.ENDC
+            print(option + " { points: " + bcolors.BOLD + str(point) + bcolors.ENDC + " }\n")
 
 
 # return points for live game // by screenshot
 def get_points_live():
-	neg= False
-	question,options=parse_question()
-	simq = ""
-	points = []
-	simq, neg = simplify_ques(question)
-	maxo=""
-	m=1 
-	if neg:
-		m=-1
-	points,maxo = google_wiki(simq, options, neg)
-	print("\n" + bcolors.UNDERLINE + question + bcolors.ENDC + "\n")
-	for point, option in zip(points, options):
-		if maxo == option.lower():
-			option=bcolors.OKGREEN+option+bcolors.ENDC
-		print(option + " { points: " + bcolors.BOLD + str(point*m) + bcolors.ENDC + " }\n")
+    neg= False
+    question,options=parse_question()
+    simq = ""
+    points = []
+    simq, neg = simplify_ques(question)
+    maxo=""
+    m=1
+    if neg:
+        m=-1
+    points,maxo = google_wiki(simq, options, neg)
+    print("\n" + bcolors.UNDERLINE + question + bcolors.ENDC + "\n")
+    for point, option in zip(points, options):
+        if maxo == option.lower():
+            option=bcolors.OKGREEN+option+bcolors.ENDC
+        print(option + " { points: " + bcolors.BOLD + str(point*m) + bcolors.ENDC + " }\n")
 
 
 # menu// main func
 if __name__ == "__main__":
-	load_json()
-	while(1):
-		keypressed = input(bcolors.WARNING +'\nPress s to screenshot live game, sampq to run against sample questions or q to quit:\n' + bcolors.ENDC)
-		if keypressed == 's':
-			get_points_live()
-		elif keypressed == 'sampq':
-			get_points_sample()
-		elif keypressed == 'q':
-			break
-		else:
-			print(bcolors.FAIL + "\nUnknown input" + bcolors.ENDC)
+    load_json()
+    while(1):
+        keypressed = input(bcolors.WARNING +'\nPress s to screenshot live game, sampq to run against sample questions or q to quit:\n' + bcolors.ENDC)
+        if keypressed == 's':
+            get_points_live()
+        elif keypressed == 'sampq':
+            get_points_sample()
+        elif keypressed == 'q':
+            break
+        else:
+            print(bcolors.FAIL + "\nUnknown input" + bcolors.ENDC)
 	
 
