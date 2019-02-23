901db7869d60f0522572fa2c003314c65d744bfe
leonardo <l.frioli@studenti.unipi.it>
2019-02-23 16:48:07 +0100

    Converted to telegram bot (I think it's the only possible solution).

--- answer_bot.py
+++ answer_bot.py
@@ -1,18 +1,15 @@
-'''
 
-	TODO:
-	* Implement normalize func
-	* Attempt to google wiki \"...\" part of question
-	* Rid of common appearances in 3 options
-	* Automate screenshot process
-	* Implement Asynchio for concurrency
 
-	//Script is in working condition at all times
-	//TODO is for improving accuracy
+# TODO:
+# * Implement normalize func
+# * Attempt to google wiki \"...\" part of question
+# * Rid of common appearances in 3 options
+# * Automate screenshot process
+# //Script is in working condition at all times
+# //TODO is for improving accuracy
 
-'''
 
-# answering bot for trivia HQ and Cash Show
+# answering bot for Live Quiz
 import json
 import urllib.request as urllib2
 from bs4 import BeautifulSoup
@@ -23,6 +20,7 @@ import argparse
 import cv2
 import os
 import pyscreenshot as Imagegrab
+import signal
 import sys
 import functools
 import np
@@ -32,6 +30,10 @@ from os.path import isfile, join
 from multiprocessing import Process, Manager
 # import wx
 from halo import Halo
+
+from telegram.ext import *
+import requests
+import re
 
 
 # for terminal colors 
@@ -52,11 +54,6 @@ remove_words = []
 # negative words
 negative_words = []
 
-# path where the smartphone is mounted
-mypath = "phone/Archivio condiviso interno/DCIM/Screenshots/"
-# number of screenshot already in this path
-screenno = 0
-
 
 def handle_exceptions(func):
     """ This function is used as a decorator to wrap the implemented method avoiding weird crashes"""
@@ -75,22 +72,21 @@ def load_settings():
     """ Loads all the configuration of the application.
 
     It loads the words to be removed and the negative words from the steeings.json file;
-    it also mount the smartphone in the phone/ dir and count the number of screenshots present
+    it also gets and returns the token for the telegram bot.
     """
     global remove_words, negative_words, screenno
     remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
     negative_words = json.loads(open("Data/settings.json").read())["negative_words"]
 
-    if not os.listdir('phone/'):
-        # the directory is empty
-        os.system("jmtpfs phone/")
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-    screenno = len(onlyfiles)
+    token = json.loads(open("Data/bottoken.json").read())["token"]
+    return token
 
 
 def show_img(str, img):
     cv2.imshow(str, img)
     cv2.waitKey(2000)
+
+    cv2.destroyAllWindows()
 
 
 def get_question(path):
@@ -133,54 +129,13 @@ def get_option(path, lineno, index):
     return imgpath
 
 
-# def crop_image(path):
-#     """Cropping the image to remove undesired stuff"""
-#     crop_config = [41/100, 45/100, 46/100]
-#     storepath = ["Screens/question.png", "Screens/answer1.png", "Screens/answer2.png", "Screens/answer3.png"]
-#
-#     image = cv2.imread(path)
-#     height, width = image.shape[:2]
-#
-#     # The problem of this huge portion of code is that the question hasn't always the same lenght
-#     # GETTING THE QUESTION
-#     # Let's get the starting pixel coordiantes (top left of cropped top)
-#     start_row, start_col = int(height * 24/100), int(0)
-#     # Let's get the ending pixel coordinates (bottom right of cropped top)
-#     end_row, end_col = int(height * 43/100), int(width)
-#
-#     cropped_img = image[start_row:end_row, start_col:end_col]
-#     # cv2.imshow("question", cropped_img)
-#     # cv2.waitKey(2000)
-#     cv2.imwrite("Screens/question.png", cropped_img)
-#
-#     question = apply_pytesseract("Screens/question.png")
-#     linenumber = len(question.splitlines())
-#     print("lines in the question: " + str(linenumber))
-#     end_q = crop_config[linenumber - 1]
-#
-#     for img in storepath[1:]:
-#         # GETTING ANSWERS
-#         start_row, start_col = int(height * end_q), int(0)
-#         end_row, end_col = int(height * (end_q + 11 / 100)), int(width)
-#
-#         cropped_img = image[start_row:end_row, start_col:end_col]
-#         # cv2.imshow(img, cropped_img)
-#         # cv2.waitKey(2000)
-#         cv2.imwrite(img, cropped_img)
-#
-#         end_q = end_q + 11/100
-#
-#     # cv2.destroyAllWindows()
-#
-#     # # GETTING THE QUESTION AND THE ASWERS
-#     # # Let's get the starting pixel coordiantes (top left of cropped top)
-#     # start_row, start_col = int(height * 27/100), int(0)
-#     # # Let's get the ending pixel coordinates (bottom right of cropped top)
-#     # end_row, end_col = int(height * 79/100), int(width)
-#     # cropped_img = image[start_row:end_row, start_col:end_col]
-#     # cv2.imwrite("Screens/cropped.png", cropped_img)
-#
-#     return question, storepath
+# # GETTING THE QUESTION AND THE ASWERS
+# # Let's get the starting pixel coordiantes (top left of cropped top)
+# start_row, start_col = int(height * 27/100), int(0)
+# # Let's get the ending pixel coordinates (bottom right of cropped top)
+# end_row, end_col = int(height * 79/100), int(width)
+# cropped_img = image[start_row:end_row, start_col:end_col]
+# cv2.imwrite("Screens/cropped.png", cropped_img)
 
 
 def apply_pytesseract(input_image):
@@ -223,21 +178,6 @@ def get_and_search_option(i, screenpath, question, lineno, negative_question, re
     option = apply_pytesseract(optionpath)
 
     google_wiki(question, option, negative_question, return_dict)
-
-
-# def read_screen(screenshot_file):
-#     """ Get OCR text //questions and options"""
-#     question, question_and_answers_paths = crop_image(screenshot_file)
-#
-#     answers = []
-#     for qa in question_and_answers_paths[1:]:
-#         answerx = apply_pytesseract(qa)
-#         answers.append(answerx)
-#
-#     print(question)
-#     print(answers)
-#
-#     return question, answers
 
 
 def simplify_ques(question):
@@ -362,11 +302,11 @@ def google_wiki(sim_ques, options, neg, return_dict):
     # return points, maxo
 
 
-#@handle_exceptions
+
+@handle_exceptions
 def solve_quiz(screenpath):
-    """Main  control flow"""
+    """Given a path to a valid screenshot it tries to solve the quiz in parallel."""
     question, lineno = get_question(screenpath)
-    # question, options = read_screen(screenpath)
     simpler_question, negative_question = simplify_ques(question)
 
     # if the answer is negative the results are resversed we check for that one with less matches
@@ -385,12 +325,6 @@ def solve_quiz(screenpath):
         proc.start()
         tasks.append(proc)
 
-    # for opt in options:
-    #     # searching in parallel
-    #     proc = Process(target=google_wiki, args=(simpler_question, opt, negative_question, return_dict))
-    #     proc.start()
-    #     tasks.append(proc)
-
     for t in tasks:
         t.join()
 
@@ -398,28 +332,48 @@ def solve_quiz(screenpath):
     # taking the max match value
     max_point = max(points)
     print("\n" + bcolors.UNDERLINE + question + bcolors.ENDC + "\n")
+    return_option = ""
     for point, option in zip(points, return_dict.keys()):
         if max_point == point:
+            return_option = option
             # if this is the "correct" answer it will appear green
             option = bcolors.OKGREEN+option+bcolors.ENDC
+
         print(option + " { points: " + bcolors.BOLD + str(point*points_coeff) + bcolors.ENDC + " }\n")
 
-
-def polling_dir():
-    """Polling the directory every tot seconds to check if a new screenshot has been added"""
-    global screenno
-    onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-    if len(onlyfiles) > screenno:
-        screenno = screenno + 1  # necessary if not deleting the file
-        solve_quiz(mypath + onlyfiles[-1])
-
-    threading.Timer(0.75, polling_dir).start()
+    return return_option
 
 
-if __name__ == "__main__":
-    load_settings()
-    print(bcolors.WARNING + "\nThe script is running | Ctrl-C to stop | DON'T FORGET TO umount phone/\n" + bcolors.ENDC)
+def hello(bot, update):
+    chat_id = update.message.chat_id
+    bot.send_message(chat_id, text="hi!")
 
-    polling_dir()
 
+def screen(bot, update):
+    chat_id = update.message.chat_id
+    file_id = update.message.photo[-1].file_id
+    newFile = bot.getFile(file_id)
+    imgpath = "Screens/screenshot.png"
+    newFile.download(imgpath)
+
+    bot.send_message(chat_id, text="Screenshot received")
+
+    answer = solve_quiz(imgpath)
+    bot.send_message(chat_id, text=answer)
+
+
+def main(ttoken):
+    updater = Updater(ttoken)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('hello', hello))
+    dp.add_handler(MessageHandler(Filters.photo, screen))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+
+    ttoken = load_settings()
+    print(bcolors.WARNING + "\nThe script/bot is running | Ctrl-C to stop \n" + bcolors.ENDC)
+
+    main(ttoken)
