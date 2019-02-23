c78ae97d952d91b4c83cf947efdd03a828ec0c1f
leonardo <l.frioli@studenti.unipi.it>
2019-02-23 18:23:11 +0100

    Added doc strings, google_wiki method cleaned.

--- answer_bot.py
+++ answer_bot.py
@@ -71,20 +71,22 @@ def handle_exceptions(func):
 def load_settings():
     """ Loads all the configuration of the application.
 
-    It loads the words to be removed and the negative words from the steeings.json file;
+    It loads the words to be removed and the negative words from the settings.json file;
     it also gets and returns the token for the telegram bot.
     """
-    global remove_words, negative_words, screenno
+    global remove_words, negative_words
     remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
     negative_words = json.loads(open("Data/settings.json").read())["negative_words"]
 
+    # get the telegram bot toekn
     token = json.loads(open("Data/bottoken.json").read())["token"]
     return token
 
 
-def show_img(str, img):
-    cv2.imshow(str, img)
-    cv2.waitKey(2000)
+def show_img(title, img):
+    """"Shows an image (Usefull for debugging)."""
+    cv2.imshow(title, img)
+    cv2.waitKey(2500)
 
     cv2.destroyAllWindows()
 
@@ -105,13 +107,23 @@ def get_question(path):
     cv2.imwrite("Screens/question.png", cropped_img)
 
     question = apply_pytesseract("Screens/question.png")
-    linenumber = len(question.splitlines())
+    question = question.splitlines()
+    linenumber = len(question)
     print("lines in the question: " + str(linenumber))
 
-    return question, linenumber
+    # returning the question without \n
+    return " ".join(question), linenumber
 
 
 def get_option(path, lineno, index):
+    """" Crops the image to get the interesting portion (that one with an option).
+
+    The image cutting depends on the line number (lineno) of the question.
+    This function just works for one option (depending on the index parameter).
+    It will be called in parallel to process all the other options."""
+    # this values derives from empirical tries
+    # they refer to the end position of the question that depends on the lineno
+    # for istance if the question has 2 lines, the options will start at crop_cropconfig[2-1]
     crop_config = [41 / 100, 45 / 100, 46 / 100]
     end_q = crop_config[lineno - 1] + (index - 1)*11/100
 
@@ -119,6 +131,7 @@ def get_option(path, lineno, index):
     height, width = image.shape[:2]
 
     start_row, start_col = int(height * end_q), int(0)
+    # they height of an option box is 11/100 of the entire image height (more or less).
     end_row, end_col = int(height * (end_q + 11 / 100)), int(width)
 
     cropped_img = image[start_row:end_row, start_col:end_col]
@@ -168,12 +181,13 @@ def apply_pytesseract(input_image):
         text = pytesseract.image_to_string(Image.open(filename))
         os.remove(filename)
 
-    # os.remove(screenshot_file)
-
     return text
 
 
 def get_and_search_option(i, screenpath, question, lineno, negative_question, return_dict):
+    """Taking the screenshot and the parsed question this function read one option and search it.
+
+     This function will be called in parallel so that the option detection and the searching is faster."""
     optionpath = get_option(screenpath, lineno, i)
     option = apply_pytesseract(optionpath)
 
@@ -206,7 +220,7 @@ def get_page(link):
         req = urllib2.Request(link, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'})
         html = urllib2.urlopen(req).read()
         return html
-    except (urllib2.URLError, urllib2.HTTPError, ValueError) as e:
+    except (urllib2.URLError, urllib2.HTTPError, ValueError):
         return ''
 
 
@@ -227,79 +241,61 @@ def split_string(source):
     return output
 
 
-# normalize points // get rid of common appearances // "quote" wiki option + ques
-def normalize():
-    return None
-
-
-# take screen shot of screen every 2 seconds and check for question
-def check_screen():
-    return None
-
-
-# wait for certain milli seconds 
-def wait(msec):
-    return None
-
-
 # answer by combining two words
-def smart_answer(content,qwords):
-    zipped= zip(qwords,qwords[1:])
-    points=0
-    for el in zipped :
-        if content.count(el[0]+" "+el[1])!=0 :
-            points+=1000
+def smart_answer(content, qwords):
+    zipped = zip(qwords, qwords[1:])
+
+    points = 0
+    for el in zipped:
+        if content.count(el[0]+" "+el[1]) != 0:
+            points += 1000
     return points
 
 
 # use google to get wiki page
 @handle_exceptions
-def google_wiki(sim_ques, options, neg, return_dict):
-    print("Searching ..." + sim_ques)
+def google_wiki(sim_ques, option, neg, return_dict):
+    """Searches the question and the single option on google and wikipedia."""
+    print("Searching ..." + sim_ques + "  " + option)
     # spinner = Halo(text='Googling and searching Wikipedia', spinner='dots2')
     # spinner.start()
     num_pages = 1
     points = 0
-    content = ""
-    maxo=""
-    maxp=-sys.maxsize
-    words = split_string(sim_ques)
-    o = options
 
-    o = o.lower()
-    original=o
-    o += ' wiki'
+    words = split_string(sim_ques)
+    searched_option = option.lower()
+
+    searched_option += ' wiki'
 
     # get google search results for option + 'wiki'
-    search_wiki = google.search(o, num_pages)
+    search_wiki = google.search(searched_option, num_pages, lang="it")
 
     if not search_wiki:
-        return_dict[options[0]] = points
+        return_dict[option] = points
         return
 
     link = search_wiki[0].link
     content = get_page(link)
-    soup = BeautifulSoup(content,"lxml")
+    soup = BeautifulSoup(content, "lxml")
     page = soup.get_text().lower()
 
-    temp=0
-
+    temp = 0
     for word in words:
          temp = temp + page.count(word)
-    temp+=smart_answer(page, words)
+
+    temp += smart_answer(page, words)
+
     if neg:
-        temp*=-1
+        temp *= -1
+
     points = temp
-    if temp>maxp:
-        maxp=temp
-        maxo=original
 
     # spinner.succeed()
     # spinner.stop()
 
-    return_dict[options] = points
+    return_dict[option] = points
     return
-    # return points, maxo
+
 
 
 
@@ -345,11 +341,13 @@ def solve_quiz(screenpath):
 
 
 def hello(bot, update):
+    """Called when the users sends /hello, just to see if the bot is alive."""
     chat_id = update.message.chat_id
     bot.send_message(chat_id, text="hi!")
 
 
 def screen(bot, update):
+    """Takes the screenshot from the telegram bot chat, saves it and calls the solve function."""
     chat_id = update.message.chat_id
     file_id = update.message.photo[-1].file_id
     newFile = bot.getFile(file_id)
@@ -359,10 +357,12 @@ def screen(bot, update):
     bot.send_message(chat_id, text="Screenshot received")
 
     answer = solve_quiz(imgpath)
+    # returning the possible answer
     bot.send_message(chat_id, text=answer)
 
 
 def main(ttoken):
+    """Starts the telegram bot registering the callback functions."""
     updater = Updater(ttoken)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('hello', hello))
@@ -372,8 +372,10 @@ def main(ttoken):
 
 
 if __name__ == '__main__':
-
     ttoken = load_settings()
+    solve_quiz("Screens/livequiz3.jpg")
+    exit(0)
+
     print(bcolors.WARNING + "\nThe script/bot is running | Ctrl-C to stop \n" + bcolors.ENDC)
 
     main(ttoken)
